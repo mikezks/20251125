043e6ea89ba98283a0c1114ab2f562e0d57b92e5
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-11-26 11:10:37 +0100

    fix: reactive search trigger

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -37,11 +37,10 @@ export class FlightSearchComponent {
 
   constructor() {
     effect(() => console.log(this.route()));
+    effect(() => this.search());
   }
 
-  protected search(filter: FlightFilter): void {
-    this.filter.set(filter);
-
+  protected search(): void {
     if (!this.filter().from || !this.filter().to) {
       return;
     }
