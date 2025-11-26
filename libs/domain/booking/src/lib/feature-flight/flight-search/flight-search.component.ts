ac54a93d642b1f077e700ffa1fd20eda02630454
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-11-26 11:49:58 +0100

    demo: fn on destroy

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, signal, untracked } from '@angular/core';
+import { Component, computed, DestroyRef, effect, inject, signal, untracked } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight } from '@flight-demo/domain/booking-api-boarding';
 import { injectTicketsFacade } from '../../logic-flight/state/facade';
@@ -19,6 +19,7 @@ import { FlightFilterComponent } from '../../ui-flight/flight-filter/flight-filt
 })
 export class FlightSearchComponent {
   private ticketsFacade = injectTicketsFacade();
+  private destroyRef = inject(DestroyRef);
 
   protected filter = signal({
     from: 'Paris',
@@ -35,11 +36,41 @@ export class FlightSearchComponent {
   protected flights$ = this.ticketsFacade.flights$;
 
   constructor() {
-    effect(() => console.log(this.route()));
+    const loggerEffectRef = effect(() => console.log(this.route()));
+    // Explicit Effect
     effect(() => {
       this.filter();
       untracked(() => this.search());
     });
+    // Manual destroy logic
+    setTimeout(() => loggerEffectRef.destroy(), 5_000);
+
+    // Signal update behavior
+    console.log(this.filter().from);
+    this.filter.update(curr => ({ ...curr, from: 'Barcelona' }));
+    console.log(this.filter().from);
+    this.filter.update(curr => ({ ...curr, from: 'Madrid' }));
+    console.log(this.filter().from);
+    this.filter.update(curr => ({ ...curr, from: 'Rome' }));
+    console.log(this.filter().from);
+    this.filter.update(curr => ({ ...curr, from: 'Athens' }));
+    console.log(this.filter().from);
+    setTimeout(() => {
+      this.filter.update(curr => ({ ...curr, from: 'Amsterdam' }));
+      console.log(this.filter().from);
+    }, 1_000);
+    console.log(this.filter().from);
+
+    // Glitch-free behavior
+    const counter = signal(0);
+    const isEven = computed(() => counter() % 2 === 0);
+    effect(() => console.log({
+      counter: untracked(() => counter()),
+      isEven: isEven()
+    }));
+    setInterval(() => counter.update(curr => curr + 1), 1_000);
+
+    this.destroyRef.onDestroy(() => console.log('Bye, bye! :('));
   }
 
   protected search(): void {
