11c01a8107dc30fef9853854fb63ab958783281f
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-11-26 16:34:20 +0100

    refactor: resource as signal forms data model

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,5 +1,5 @@
 import { httpResource } from '@angular/common/http';
-import { Component, input, numberAttribute, signal } from '@angular/core';
+import { Component, input, numberAttribute } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Field, form } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
@@ -17,23 +17,18 @@ import { initialPassenger, Passenger } from '../../logic-passenger/model/passeng
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
-  // (1) Data Model: Writable Signal
-  protected readonly passenger = signal({
-    ...initialPassenger,
-    firstName: 'Sarah'
-  });
-
-  // (2) Field State: value, valid, dirty, touched, readonly, hidden, etc.
-  protected readonly editForm = form(this.passenger);
-
   readonly id = input(0, { transform: numberAttribute });
+
+  // (1) Data Model: Writable Signal
   protected readonly passengerResource = httpResource<Passenger>(() => ({
     url: 'https://demo.angulararchitects.io/api/passenger',
     params: { id: this.id() }
   }), { defaultValue: initialPassenger });
 
+  // (2) Field State: value, valid, dirty, touched, readonly, hidden, etc.
+  protected readonly editForm = form(this.passengerResource.value);
+
   protected save(): void {
-    console.log(this.editForm().value());
-    this.passengerResource.set(this.editForm().value());
+    console.log(this.passengerResource.value());
   }
 }
