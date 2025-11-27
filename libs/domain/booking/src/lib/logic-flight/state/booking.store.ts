c1292ea27ba206bfb52294d32b0125c05b849b50
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-11-27 13:38:57 +0100

    refactor: separates state definition, updater and side-effects, with props di access

--- libs/domain/booking/src/lib/logic-flight/state/booking.store.ts
+++ libs/domain/booking/src/lib/logic-flight/state/booking.store.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { tapResponse } from '@ngrx/operators';
-import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from '@ngrx/signals';
+import { patchState, signalStore, withComputed, withHooks, withMethods, withProps, withState } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { pipe, switchMap } from 'rxjs';
 import { FlightService } from '../data-access/flight.service';
@@ -8,27 +8,38 @@ import { Flight } from '../model/flight';
 import { FlightFilter } from '../model/flight-filter';
 
 
+export interface BookingState {
+  filter: FlightFilter;
+  basket: Record<number, boolean>;
+  flights: Flight[];
+}
+
+export const initalBookingState: BookingState = {
+  filter: {
+    from: 'London',
+    to: 'New York',
+    urgent: false
+  },
+  basket: {
+    3: true,
+    5: true,
+  },
+  flights: [],
+};
+
+
 export const BookingStore = signalStore(
   { providedIn: 'root' },
-  withState({
-    filter: {
-      from: 'London',
-      to: 'New York',
-      urgent: false
-    },
-    basket: {
-      3: true,
-      5: true,
-    } as Record<number, boolean>,
-    flights: [] as Flight[],
-  }),
+  // State
+  withState(initalBookingState),
   withComputed(store => ({
     delayedFlights: () => store.flights().filter(flight => flight.delayed),
   })),
-  withMethods((
-    store,
-    flightService = inject(FlightService)
-  ) => ({
+  withProps(() => ({
+    _flightService: inject(FlightService)
+  })),
+  // Updater
+  withMethods(store => ({
     setFilter: (filter: FlightFilter) => patchState(store, { filter }),
     setFlights: (flights: Flight[]) => patchState(store, { flights }),
     updateBasket: (id: number, selected: boolean) => patchState(store, state => ({
@@ -37,20 +48,24 @@ export const BookingStore = signalStore(
         [id]: selected
       }
     })),
+  })),
+  // Side-Effects
+  withMethods(store => ({
     loadFlights: rxMethod<FlightFilter>(pipe(
-      switchMap(filter => flightService.find(
+      switchMap(filter => store._flightService.find(
         filter.from, filter.to, filter.urgent
       ).pipe(
         tapResponse({
-          next: flights => patchState(store, { flights }),
+          next: flights => store.setFlights(flights),
           error: err => console.error(err)
         }),
       )),      
     )),
     loadFlightById: rxMethod<number>(pipe(
-      // TODO: 
+      // TODO: implements loading topic
     )),
   })),
+  // Store Lifecycle Hooks
   withHooks(store => ({
     onInit: () => store.loadFlights(store.filter),
   }))
